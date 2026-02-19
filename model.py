12cf48a9ffc93af5024047495fa87c9df219e16d
Alex <antranthien69@gmail.com>
2026-02-19 17:04:07 -0500

    Added Pydantic Validation

---
+++ model.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "service": "Environment Telemetry & Emission Backend",
+        "status": "running"
+    }
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+class ElectricityReading(BaseModel):
+    sensor_id: str
+    timestamp: datetime
+    kwh: float
+    unit: str
+
+
+@app.post("/readings")
+def ingest_reading(reading: ElectricityReading):
+    return {
+        "message": "Reading accepted",
+        "data": reading
+    }
